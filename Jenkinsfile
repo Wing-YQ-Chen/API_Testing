a14130535228dfd74016d2f34d6660d6b5693531
Wing Chen <13059381952@163.com>
2025-02-24 22:41:14 +0800

    ++

--- Jenkinsfile
+++ Jenkinsfile
@@ -27,7 +27,7 @@ pipeline {
 			}
 			post {
 				always {
-					archiveArtifacts artifacts: 'Reports/**', allowEmptyArchive: true
+					archiveArtifacts artifacts: 'Reports/allure-report/index.html', allowEmptyArchive: true
 				}
 			}
 		}
--- script/main.py
+++ script/main.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import shutil
 import time
 import pytest
 
@@ -11,13 +10,14 @@ import pytest
 if __name__ == "__main__":
     report_dir = r'..\Reports'
     allure_dir = os.path.join(report_dir, 'allure-results')
+    report_dir = os.path.abspath(os.path.join(report_dir, 'allure-report'))
     allure_dir = os.path.abspath(allure_dir)
-    report_dir = os.path.abspath(report_dir)
 
     # 添加 common 模块所在的路径到 sys.path，否则pytest在终端运行时找不到common模块
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-    shutil.rmtree(allure_dir, True)
     pytest.main(["--reruns=0", "--reruns-delay=2", '--alluredir', allure_dir])
-    os.system(f'allure generate "{allure_dir}" --clean --single-file --lang en --name "API Testing" -o "{report_dir}"')
-    # os.system(f'allure open "{report_dir}"')
+    os.system(f'allure generate "{allure_dir}" '
+              f'--report-dir "{report_dir}" '
+              f'--name "API Testing" '
+              f'--clean --single-file --lang en')
