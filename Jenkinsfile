13d5743e94b1287a4b9e57bfc1af47305cda52dd
Wing Chen <13059381952@163.com>
2025-02-24 20:20:29 +0800

    ++

---
+++ Jenkinsfile
@@ -0,0 +1,35 @@
+pipeline {
+	agent any
+    stages {
+
+		stage('Build') {
+			steps {
+				bat """
+				echo "Building"
+                python -m venv venv
+                call venv\\Scripts\\activate。
+                pip list
+                pip install -r requirements.txt -i "https://mirrors.aliyun.com/pypi/simple/"
+                pip list
+                """
+
+            }
+        }
+
+
+		stage('Run Testing') {
+			steps {
+				bat """
+				echo "Run Testing"
+				python script\\main.py
+				"""
+			}
+			post {
+				always {
+					archiveArtifacts artifacts: 'Reports/index.html', allowEmptyArchive: true
+				}
+			}
+		}
+
+    }
+}
--- script/main.py
+++ script/main.py
@@ -1,3 +1,6 @@
+import sys
+import io
+
 import os
 import shutil
 import time
@@ -5,16 +8,17 @@ import pytest
 
 # https://reqres.in/api-docs/#/
 # https://httpbin.org/#/
-
+# https://allurereport.org/docs/install-for-windows/
 
 if __name__ == "__main__":
-    allure_dir = '../Reports/allure-results'
-    report_dir = os.path.join("../Reports", time.strftime(r'%Y%m%d%H%M%S', time.localtime()).__str__())
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+    report_dir = '..\Reports'
+    allure_dir = os.path.join(report_dir, 'allure-results')
     allure_dir = os.path.abspath(allure_dir)
     report_dir = os.path.abspath(report_dir)
 
     shutil.rmtree(allure_dir, True)
     pytest.main(["--reruns=0", "--reruns-delay=1", '--alluredir', allure_dir])
     os.system(f'allure generate "{allure_dir}" --clean --single-file --lang en --name "API Testing" -o "{report_dir}"')
-    os.system(f'allure open "{report_dir}"')
-
+    # os.system(f'allure open "{report_dir}"')
