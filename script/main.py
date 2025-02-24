a86d9e4be7d063313e97285cf5b7a8b0d749973b
Wing Chen <13059381952@163.com>
2025-02-24 21:36:37 +0800

    ++

--- script/main.py
+++ script/main.py
@@ -1,6 +1,4 @@
 import sys
-import io
-
 import os
 import shutil
 import time
@@ -15,6 +13,9 @@ if __name__ == "__main__":
     allure_dir = os.path.join(report_dir, 'allure-results')
     allure_dir = os.path.abspath(allure_dir)
     report_dir = os.path.abspath(report_dir)
+
+    # 添加 common 模块所在的路径到 sys.path，否则pytest在终端运行时找不到common模块
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
     shutil.rmtree(allure_dir, True)
     pytest.main(["--reruns=0", "--reruns-delay=2", '--alluredir', allure_dir])
